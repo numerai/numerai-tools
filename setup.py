36e952ef61eb9bb14d3dc0cc190a62bba7d69978
Noah Harasz <ndharasz@live.com>
2023-10-09 10:45:29 -0700

    loosen package constraints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 
 def load(path):
@@ -37,9 +37,9 @@ if __name__ == "__main__":
         package_data={'numerai': ['LICENSE', 'README.md']},
         packages=find_packages(exclude=['tests']),
         install_requires=[
-            "pandas==1.3.5",
-            "numpy==1.21.6",
-            "scipy==1.2.1",
-            "sklearn==0.0",
+            "pandas~=1.3.5",
+            "numpy~=1.21.6",
+            "scipy~=1.2.1",
+            "sklearn~=0.0",
         ],
     )
