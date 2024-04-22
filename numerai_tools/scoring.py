7882737a5efb43ded128601b84c98388e589adee
Noah Harasz <ndharasz@live.com>
2024-04-22 14:50:03 -0700

    attempt neutralization speedup

--- numerai_tools/scoring.py
+++ numerai_tools/scoring.py
@@ -317,8 +317,8 @@ def neutralize(
         # add a column of 1s to the neutralizer array in case neutralizer_arr is a single column
         (neutralizer_arr, np.array([1] * len(neutralizer_arr)).reshape(-1, 1))
     )
-    inverse_neutralizers = np.linalg.pinv(neutralizer_arr, rcond=1e-6)
-    adjustments = proportion * neutralizer_arr.dot(inverse_neutralizers.dot(df_arr))
+    least_squares = np.linalg.lstsq(neutralizer_arr, df_arr, rcond=1e-6)[0]
+    adjustments = proportion * neutralizer_arr.dot(least_squares)
     neutral = df_arr - adjustments
     return pd.DataFrame(neutral, index=df.index, columns=df.columns)
 
