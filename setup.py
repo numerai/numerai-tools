bfb5b070dad3dd8b3642720d46d60af0576b5258
Carlo Lepelaars <info@carlolepelaars.nl>
2024-05-23 16:58:38 +0200

    Relax Pandas version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         package_data={"numerai": ["LICENSE", "README.md"]},
         packages=find_packages(exclude=["tests"]),
         install_requires=[
-            "pandas>=1.3.1, <=2.1.3",
+            "pandas>=1.3.1",
             "numpy~=1.26.2",
             "scipy~=1.11.4",
             "scikit-learn>=1.3.0",
