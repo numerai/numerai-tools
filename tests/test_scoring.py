61ade8ce3a5c306fe3bae8b2c25064fc15cf514d
Noah Harasz <ndharasz@live.com>
2024-08-15 17:57:31 -0700

    add top_bottom=None type error test case

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -208,6 +208,8 @@ class TestScoring(unittest.TestCase):
         assert pd.Series(s).equals(df["target"]), f"{s} != {list(df['target'].values)}"
 
     def test_filter_top_bottom(self):
+        with self.assertRaises(TypeError):
+            filter_sort_top_bottom(self.up, top_bottom=None)
         assert np.isclose(
             filter_sort_top_bottom(self.up, top_bottom=2).values.T, [0, 1, 3, 4]
         ).all()
