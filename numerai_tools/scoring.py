8ebb97e9fbd48b6351555ac8006b29494be44d93
Noah Harasz <ndharasz@live.com>
2024-09-10 13:46:27 -0700

    add more testing around neutralization

--- numerai_tools/scoring.py
+++ numerai_tools/scoring.py
@@ -307,6 +307,7 @@ def neutralize(
     Returns:
         pd.DataFrame - the neutralized data
     """
+    assert not df.isna().any().any(), "Data contains NaNs"
     assert not neutralizers.isna().any().any(), "Neutralizers contain NaNs"
     assert len(df.index) == len(neutralizers.index), "Indices don't match"
     assert (df.index == neutralizers.index).all(), "Indices don't match"
@@ -317,8 +318,11 @@ def neutralize(
         # add a column of 1s to the neutralizer array in case neutralizer_arr is a single column
         (neutralizer_arr, np.array([1] * len(neutralizer_arr)).reshape(-1, 1))
     )
+    print(neutralizer_arr)
     least_squares = np.linalg.lstsq(neutralizer_arr, df_arr, rcond=1e-6)[0]
+    print(least_squares)
     adjustments = proportion * neutralizer_arr.dot(least_squares)
+    print(adjustments)
     neutral = df_arr - adjustments
     return pd.DataFrame(neutral, index=df.index, columns=df.columns)
 
